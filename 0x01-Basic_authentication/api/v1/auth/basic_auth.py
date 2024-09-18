253590df61e0d025988d0cd9f04cd150f60abc23
theideabased <ogunmusireseyi01@gmail.com>
2024-09-18 23:51:13 +0000

    first commit

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -20,3 +20,23 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        if base64_authorization_header is None:
+            return None
+        elif type(base64_authorization_header) != str:
+            return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) != str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        dba_header = decoded_base64_authorization_header.split(":", 1)
+        if len(dba_header) != 2:
+            return None,None
+            
+        return dba_header[0], dba_header[1]
