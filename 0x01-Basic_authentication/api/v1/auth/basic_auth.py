5912bcf45ea2d9b153c46f951275f5c001e82f56
theideabased <ogunmusireseyi01@gmail.com>
2024-09-18 22:33:45 +0000

    basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,16 @@ basic authentication
 
 
 class BasicAuth(Auth):
-    pass
+    """
+    this authentication for basic authentication 
+    in flask
+    """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        elif type(authorization_header) != str:
+            return None
+        elif authorization_header.startswith('Basic ') != True:
+            return None
+        else:
+            return authorization_header[6:]
