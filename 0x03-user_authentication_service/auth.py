268bc6550e1ff3bc829b53d43532a94d5d231e0e
theideabased <ogunmusireseyi01@gmail.com>
2024-09-28 01:15:07 +0000

    authenticating user with salt and hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """ This is a function that will be used for auth """
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
+user = User()
 
 
 def _hash_password(password : str):
@@ -12,3 +16,22 @@ def _hash_password(password : str):
     hash = bcrypt.hashpw(bytes, salt)
 
     return hash
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str):
+        """ this will find if a user is already registered
+        and if it is not registered it will hash the password and 
+        store it in the database"""
+        try:
+            x = self._db.find_user_by(email=email)
+            raise ValueError(f"User {x.email} already exists")
+        except NoResultFound:
+            hash_pass = _hash_password(password)
+            self._db.add_user(email, hash_pass)
+            return user
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -57,13 +57,10 @@ class DB:
             if key not in valid_columns:
                 raise InvalidRequestError()
 
-        try:
-            user = self.__session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound()
-            return user
-        except NoResultFound:
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
             raise NoResultFound()
+        return user
 
 
 
